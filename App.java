e1efca890041a0990a20800c3727ca63ebfd4499
Aldrich Homer <aldihomer15@gmail.com>
2026-09-03 15:09:15 +0700

    update code

--- App.java
+++ App.java
@@ -1,9 +1,21 @@
-abstract class Hewan {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+interface Movable {
+    String move();
+}
+
+interface Feedable {
+    void eat();
+}
+
+abstract class Animal {
     private String name;
     private int age;
     private String food;
 
-    Hewan(String name, int age, String food) {
+    Animal(String name, int age, String food) {
         this.name = name;
         this.age = age;
         this.food = food;
@@ -31,15 +43,17 @@ abstract class Hewan {
 
     public abstract void printSound();
 
+    public abstract String getJenisHewan();
+
     void printData() {
-        System.out.println("Jenis Hewan: " + getClass().getSimpleName());
-        System.out.println("Nama: " + name);
-        System.out.println("Umur: " + age + " tahun");
-        System.out.println("Makanan: " + food);
+        System.out.println("Animal Type: " + getJenisHewan());
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age + " tahun");
+        System.out.println("Food: " + food);
     }
 }
 
-abstract class HewanDarat extends Hewan {
+abstract class LandAnimal extends Animal implements Movable, Feedable {
     private int numberOfLegs;
 
     void setNumberOfLLegs(int numberOfLegs) {
@@ -49,55 +63,99 @@ abstract class HewanDarat extends Hewan {
         return numberOfLegs;
     }
 
-    HewanDarat(String name, int age, String food, int numberOfLegs) {
+    LandAnimal(String name, int age, String food, int numberOfLegs) {
         super(name, age, food);
         this.numberOfLegs = numberOfLegs;
-    }
-    
-    public void move() {
-        System.out.println("Hewan darat tersebut berjalan menggunakan 4 kaki");
     }
 
     @Override
     void printData() {
         super.printData();
-        System.out.println("Jumlah Kaki: " + getNumberOfLegs());
-        move();
+        System.out.println("Legs: " + getNumberOfLegs());
+        System.out.println(move());
+        eat();
     }
 }
 
-class Anjing extends HewanDarat {
-    Anjing(String name, int age, String food, int numberOfLegs) {
+class Dog extends LandAnimal {
+    Dog(String name, int age, String food, int numberOfLegs) {
         super(name, age, food, numberOfLegs);
+    }
+
+    @Override
+    public String getJenisHewan() {
+        return "Dog";
     }
     
     @Override
     public void printSound() {
-        System.out.println("Suara: Guk Guk");
+        System.out.println("Sound: Woof Woof");
+    }
+
+    @Override
+    public String move() {
+        return "Movement: Dog is running.";
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Eating: Dog is eating " + getFood() + ".");
     }
 }
-class Kucing extends HewanDarat {
-    Kucing(String name, int age, String food, int numberOfLegs) {
+
+class Cat extends LandAnimal {
+    Cat(String name, int age, String food, int numberOfLegs) {
         super(name, age, food, numberOfLegs);
+    }
+
+    @Override
+    public String getJenisHewan() {
+        return "Cat";
     }
     
     @Override
     public void printSound() {
         System.out.println("Suara: Meow Meow");
     }
+
+    @Override
+    public String move() {
+        return "Movement: Cat is stalking and walking softly.";
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Eating: Cat is eating " + getFood() + ".");
+    }
 }
-class Tikus extends HewanDarat {
-    Tikus(String name, int age, String food, int numberOfLegs) {
+
+class Mouse extends LandAnimal {
+    Mouse(String name, int age, String food, int numberOfLegs) {
         super(name, age, food, numberOfLegs);
+    }
+
+    @Override
+    public String getJenisHewan() {
+        return "Mouse";
     }
     
     @Override
     public void printSound() {
-        System.out.println("Suara: Cit Cit");
+        System.out.println("Suara: Cit Cit / Squeak");
+    }
+
+    @Override
+    public String move() {
+        return "Movement: Mouse is scurrying.";
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Eating: Mouse is eating " + getFood() + ".");
     }
 }
 
-abstract class HewanLaut extends Hewan {
+abstract class SeaAnimal extends Animal implements Movable, Feedable {
     private double depth;
 
     void setDepth(double depth) {
@@ -107,7 +165,7 @@ abstract class HewanLaut extends Hewan {
         return depth;
     }
 
-    HewanLaut(String name, int age, String food, double depth) {
+    SeaAnimal(String name, int age, String food, double depth) {
         super(name, age, food);
         this.depth = depth;
     }
@@ -115,42 +173,91 @@ abstract class HewanLaut extends Hewan {
     @Override
     void printData() {
         super.printData();
-        System.out.println("Depth: " + getDepth());
+        System.out.println("Max Depth: " + getDepth());
+        System.out.println(move());
+        eat();
     }
 }
 
-class Ikan extends HewanLaut {
-    Ikan(String name, int age, String food, double depth) {
+class Fish extends SeaAnimal {
+    Fish(String name, int age, String food, double depth) {
         super(name, age, food, depth);
+    }
+
+    @Override
+    public String getJenisHewan() {
+        return "Fish";
     }
     
     @Override
     public void printSound() {
         System.out.println("Suara: Blub Blub");
     }
-}
-class Hiu extends HewanLaut {
-    Hiu(String name, int age, String food, double depth) {
-        super(name, age, food, depth);
-    }
-    
+
     @Override
-    public void printSound() {
-        System.out.println("Suara: Splash / Silent Predator");
+    public String move() {
+        return "Movement: Fish is swimming rapidly.";
     }
-}
-class Paus extends HewanLaut {
-    Paus(String name, int age, String food, double depth) {
-        super(name, age, food, depth);
-    }
-    
+
     @Override
-    public void printSound() {
-        System.out.println("Suara: Echolocation Click / Whistle");
+    public void eat() {
+        System.out.println("Eating: Fish is eating " + getFood() + ".");
     }
 }
 
-abstract class HewanUdara extends Hewan {
+class Shark extends SeaAnimal {
+    Shark(String name, int age, String food, double depth) {
+        super(name, age, food, depth);
+    }
+
+    @Override
+    public String getJenisHewan() {
+        return "Shark";
+    }
+    
+    @Override
+    public void printSound() {
+        System.out.println("Suara: Silent Splash");
+    }
+
+    @Override
+    public String move() {
+        return "Movement: Shark is gliding through the deep ocean.";
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Eating: Shark is eating " + getFood() + ".");
+    }
+}
+
+class Whale extends SeaAnimal {
+    Whale(String name, int age, String food, double depth) {
+        super(name, age, food, depth);
+    }
+
+    @Override
+    public String getJenisHewan() {
+        return "Whale";
+    }
+    
+    @Override
+    public void printSound() {
+        System.out.println("Suara: Whale Song / Click");
+    }
+
+    @Override
+    public String move() {
+        return "Movement: Whale is diving gracefully.";
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Eating: Whale is eating " + getFood() + ".");
+    }
+}
+
+abstract class AirAnimal extends Animal implements Movable, Feedable {
     private double wingSpan;
 
     void setWingSpan(double wingSpan) {
@@ -160,7 +267,7 @@ abstract class HewanUdara extends Hewan {
         return wingSpan;
     }
 
-    HewanUdara(String name, int age, String food, double wingSpan) {
+    AirAnimal(String name, int age, String food, double wingSpan) {
         super(name, age, food);
         this.wingSpan = wingSpan;
     }
@@ -168,61 +275,150 @@ abstract class HewanUdara extends Hewan {
     @Override
     void printData() {
         super.printData();
-        System.out.println("Depth: " + getWingSpan());
+        System.out.println("Max Depth: " + getWingSpan());
+        System.out.println(move());
+        eat();
     }
 }
 
-class Burung extends HewanLaut {
-    Burung(String name, int age, String food, double wingSpan) {
+class Eagle extends AirAnimal {
+    Eagle(String name, int age, String food, double wingSpan) {
         super(name, age, food, wingSpan);
     }
-    
+
     @Override
-    public void printSound() {
-        System.out.println("Suara: Cuit Cuit");
-    }
-}
-class Elang extends HewanLaut {
-    Elang(String name, int age, String food, double wingSpan) {
-        super(name, age, food, wingSpan);
+    public String getJenisHewan() {
+        return "Eagle";
     }
     
     @Override
     public void printSound() {
         System.out.println("Suara: Kreeee / Screech");
     }
+    
+    @Override
+    public String move() {
+        return "Movement: Eagle is soaring high in the sky.";
+    }
+    
+    @Override
+    public void eat() {
+        System.out.println("Eating: Eagle is eating " + getFood() + ".");
+    }
 }
-class Bebek extends HewanLaut {
-    Bebek(String name, int age, String food, double wingSpan) {
+
+class Parrot extends AirAnimal {
+    Parrot(String name, int age, String food, double wingSpan) {
         super(name, age, food, wingSpan);
+    }
+
+    @Override
+    public String getJenisHewan() {
+        return "Parrot";
     }
     
     @Override
     public void printSound() {
-        System.out.println("Suara: Kwek Kwek");
+        System.out.println("Suara: Squawk / Echo Words");
+    }
+
+    @Override
+    public String move() {
+        return "Movement: Parrot is fluttering through trees.";
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Eating: Parrot is eating " + getFood() + ".");
+    }
+}
+
+class Duck extends AirAnimal {
+    Duck(String name, int age, String food, double wingSpan) {
+        super(name, age, food, wingSpan);
+    }
+
+    @Override
+    public String getJenisHewan() {
+        return "Duck";
+    }
+    
+    @Override
+    public void printSound() {
+        System.out.println("Suara: Quack Quack");
+    }
+
+    @Override
+    public String move() {
+       return "Movement: Duck is flying and paddling.";
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Eating: Duck is eating " + getFood() + ".");
     }
 }
 
 public class App {
     public static void main(String[] args) {
-        Hewan[] daftarHewan = new Hewan[] {
-            new Anjing("Doggy", 3, "Tulang", 4),
-            new Kucing("Milo", 2, "Ikan", 4),
-            new Tikus("Jerry", 1, "Keju", 4),
 
-            new Ikan("Nemo", 1, "Plankton", 15.0),
-            new Hiu("Megalo", 10, "Daging", 500.0),
-            new Paus("Bluey", 20, "Krill", 1000.0),
+        ArrayList<Animal> animals = new ArrayList<>();
 
-            new Burung("Pipit", 1, "Biji-bijian", 0.2),
-            new Elang("Garuda", 5, "Daging", 2.1),
-            new Bebek("Donald", 2, "Dedak", 0.8)
+        Animal[] daftarHewan = new Animal[] {
+            new Dog("Doggy", 3, "Meat", 4),
+            new Cat("Milo", 2, "Fish", 4),
+            new Mouse("Jerry", 1, "Cheese", 4),
+            
+            new Fish("Nemo", 1, "Plankton", 15.0),
+            new Shark("Megalo", 10, "Meat", 500.0),
+            new Whale("Bluey", 20, "Krill", 1000.0),
+
+            new Eagle("Garuda", 5, "Meat", 2.1),
+            new Parrot("Philip", 1, "Seeds", 0.2),
+            new Duck("Donald", 2, "Bran", 0.8)
         };
+        
+        System.out.println("========================================");
+        System.out.println("=== DATA SEMUA HEWAN (FOR-EACH LOOP) ===");
+        System.out.println("========================================");
 
-        for (Hewan h : daftarHewan) {
+        for (Animal h : daftarHewan) {
             h.printData();
             h.printSound();
+            animals.add(h);
             System.out.println("----------------------------------------");
+        }
+        System.out.println();
+        
+        System.out.println("========================================");
+        System.out.println("=== HEWAN SORTED BY NAME (ASCENDING) ===");
+        System.out.println("========================================");
+
+        Collections.sort(animals, new Comparator<Animal>() {
+            @Override
+            public int compare(Animal a1, Animal a2) {
+                return a1.getName().compareToIgnoreCase(a2.getName());
+            }
+        });
+
+        for (int i = 0; i < 9; i++) {
+            System.out.println((i + 1) + ". " + animals.get(i).getName() + " (" + animals.get(i).getJenisHewan() + ", " + animals.get(i).getAge() + " tahun)");
+        }
+        System.out.println();
+
+        System.out.println("========================================");
+        System.out.println("=== HEWAN SORTED BY AGE (ASCENDING) ===");
+        System.out.println("========================================");
+
+        Collections.sort(animals, new Comparator<Animal>() {
+            @Override
+            public int compare(Animal a1, Animal a2) {
+            return Integer.compare(a1.getAge(), a2.getAge());
+            }
+        });
+
+        for (int i = 0; i < 9; i++) {
+            System.out.println((i + 1) + ". " + animals.get(i).getName() + " - " + animals.get(i).getAge() + " tahun");
         }
     }
 }
